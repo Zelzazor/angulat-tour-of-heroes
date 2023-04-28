479fdf21e6a2d678a65096b0c98b6275a2c01043
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2023-04-28 10:07:34 -0400

    implementation of heroes and messages services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
+import { HeroService } from './../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
-
+export class HeroesComponent implements OnInit {
+  
+  heroes: Hero[] = [];
   public selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService){}
 
   onSelect(hero: Hero): void {
     if(this.selectedHero?.id === hero.id) {
@@ -18,5 +22,14 @@ export class HeroesComponent {
       return;
     }
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
