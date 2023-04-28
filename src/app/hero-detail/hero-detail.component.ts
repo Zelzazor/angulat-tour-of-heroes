e5ac7d28801c9ab812ea80a297bc1e4b674a11ae
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2023-04-28 10:58:02 -0400

    http mocking with observables and subjects

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -16,6 +16,13 @@ export class HeroDetailComponent implements OnInit {
     this.getHero();
   }
 
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
   goBack(): void {
     this.location.back();
   }
